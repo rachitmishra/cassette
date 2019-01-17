9c05c11a93294e5bc5d0727777980b684d9f81be
Rachit Mishra <twntee@gmail.com>
2019-01-17 06:14:42 +0530

    Cassette FP

---
+++ app/src/main/java/com/raywenderlich/android/cassette/SongStore.kt
@@ -0,0 +1,63 @@
+package com.raywenderlich.android.cassette
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+import androidx.core.content.edit
+
+
+/**
+ * Helper to store and retrieve songs from preferences
+ */
+class SongStore private constructor(context: Context) {
+
+  private val preferences: SharedPreferences =
+      PreferenceManager.getDefaultSharedPreferences(context)
+
+  val allSongs: Set<String>
+    get() = getStoredSongs()
+
+  companion object {
+
+    /**
+     * store preference key
+     */
+    const val STORE_KEY: String = "songs"
+
+    /**
+     * Factory method to create new store instance
+     *
+     * @param context Context for new store
+     */
+    fun from(context: Context): SongStore = SongStore(context)
+  }
+
+  /**
+   * Get all songs
+   *
+   */
+  private fun getStoredSongs(): MutableSet<String> {
+    return preferences.getStringSet(STORE_KEY, mutableSetOf<String>()) ?: mutableSetOf()
+  }
+
+  /**
+   * Save a song to store
+   *
+   * @param song song in CSV format (title, artist, year)
+   * @param onSuccess function callback to handle success
+   * @param onError function callback to handle error
+   */
+  fun saveSong(song: String, onSuccess: (() -> Unit)?, onError: (Int) -> Unit) {
+    val songs = getStoredSongs()
+
+    if (songs.contains(song)) {
+      onError(R.string.error_song_exists)
+    } else {
+      songs.add(song)
+      preferences.edit(true) {
+        putStringSet(STORE_KEY, songs)
+      }
+      onSuccess?.invoke()
+    }
+  }
+}
